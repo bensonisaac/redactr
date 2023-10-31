1f1007314a4469bf20cd74fd27989cb1f45e21ec
Benson "Santi" Isaac <131260531+bensonisaac@users.noreply.github.com>
2023-10-31 19:59:42 +0100

    Copy funtionality implemented.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ document.getElementById("redactForm").addEventListener("submit", (e) => {
 });
 
 const redactFunction = () => {
+  let redactText = document.getElementById("redatedText")
+  let redactOptionValue = document.getElementById("choice").value;
   let originalTextValue = originalText.value;
   const wordsToRedactValue = wordsToRedact.value
     .split(/[;,|]/)
@@ -21,13 +23,19 @@ const redactFunction = () => {
       wordsToRedactValue.forEach((word) => {
         if (redact.includes(word)) {
           let regexText = new RegExp(word, "gi");
-          redact = redact.replace(regexText, "****");
+          redact = redact.replace(regexText, redactOptionValue);
         } else {
           alert("Not found");
         }
       });
 
-      document.getElementById("redatedText").innerHTML = redact;
+      redactText.innerText = redact
+      if(redact){
+        document.getElementById("redatedTextContainer").addEventListener("dblclick", () => {
+          navigator.clipboard.writeText(redact)
+        });
+      }
+     
     } else {
       alert("Words to be blurred out is empty");
     }
