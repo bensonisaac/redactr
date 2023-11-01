fd48a35597a0b9e12f61f1e0d59e80683f32072c
Benson "Santi" Isaac <131260531+bensonisaac@users.noreply.github.com>
2023-11-01 18:26:16 +0100

    Bugs fixed

--- script.js
+++ script.js
@@ -7,37 +7,69 @@ document.getElementById("redactForm").addEventListener("submit", (e) => {
 });
 
 const redactFunction = () => {
-  let redactText = document.getElementById("redatedText")
+  let redactTextContainer = document.getElementById("redatedTextContainer");
   let redactOptionValue = document.getElementById("choice").value;
   let originalTextValue = originalText.value;
+
+  // gets the words and turn them into strings
+  // using map to trim for extra whitespaces
   const wordsToRedactValue = wordsToRedact.value
     .split(/[;,|]/)
     .map((e) => e.trim());
+
   let redact = originalTextValue;
+
+  // turn the original words into strings
   let redactWords = redact.split(/[" ";,|]/);
 
+  // check if they are upto 10 or empty
   if (!redact || redactWords.length <= 10) {
-    alert("Text should be more than 10.");
+    
+    document.getElementById("textError").innerText =
+      "Text should be more than 10.";
   } else {
     if (wordsToRedactValue) {
+      // iterates through the word
       wordsToRedactValue.forEach((word) => {
+        // check if the word is in the original words
+        // replace anyone found
         if (redact.includes(word)) {
           let regexText = new RegExp(word, "gi");
           redact = redact.replace(regexText, redactOptionValue);
+          redactTextContainer.innerHTML = `
+      <p id="redatedText">
+      <button class="copy-icon" id="copier">&#128203;</button>
+      <span class="copy-message" id="copyMessage">Click to copy</span>
+      ${redact}</p>
+      `;
         } else {
-          alert("Not found");
+          document.getElementById(
+            "wordError"
+          ).innerText = `${word} is not found`;
+          return;
         }
       });
 
-      redactText.innerText = redact
-      if(redact){
-        document.getElementById("redatedTextContainer").addEventListener("dblclick", () => {
-          navigator.clipboard.writeText(redact)
+      // check the redacted words and let the user copies it if they
+      // double click in the container
+      if (redact) {
+        const copier = document.getElementById("copier");
+        const copyMessage = document.getElementById("copyMessage");
+        copier.addEventListener("mouseover", () => {
+          copyMessage.style.display = "block";
+        });
+        copier.addEventListener("mouseout", () => {
+          copyMessage.style.display = "none";
+        });
+        copier.addEventListener("click", () => {
+          navigator.clipboard.writeText(redact);
+          alert("Copied!👌");
         });
       }
-     
     } else {
-      alert("Words to be blurred out is empty");
+      document.getElementById(
+        "wordError"
+      ).innerText = `Words to be blurred out is empty`;
     }
   }
 };
